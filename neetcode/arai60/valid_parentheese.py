122559fd6c7bf1d588578b8e55ee5e5c9eb10440
helloooooo <yomicerisier@gmail.com>
2025-06-03 07:56:25 +0900

    fix alog

--- neetcode/arai60/valid_parentheese.py
+++ neetcode/arai60/valid_parentheese.py
@@ -1,34 +1,16 @@
 import unittest
 class Solution:
-    def isValid(self, s: str) -> bool:
-        parthness_stack = []
+  def isValid(self, s: str) -> bool:
+    parthness_stack = []
+    parthness_dict = dict(('()', '{}', '[]'))
 
-        for c in s:
-            if c == '(' or c == '{' or c == '[':
-                 print('call?')
-                 parthness_stack.append(c)
-            else:
-                if not parthness_stack:
-                    return False
-                compare_char = parthness_stack.pop()
-                if c == ')':
-                  if compare_char == '(':
-                      next
-                  else:
-                      return False
-                if c == '}':
-                  if compare_char == '{':
-                      next
-                  else:
-                      return False
-                if c == ']':
-                  if compare_char == '[':
-                      next
-                  else:
-                      print(compare_char)
-                      return False
-        return not parthness_stack and True
-    
+    for c in s:
+        if c in '({[':
+           parthness_stack.append(c)
+        elif not parthness_stack or c != parthness_dict[parthness_stack.pop()]:
+           return False
+        
+    return not parthness_stack
 
 class TestValidParentheses(unittest.TestCase):
     def setUp(self):
