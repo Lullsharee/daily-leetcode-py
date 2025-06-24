a2cb51f6b0a8441f99616f5c85112f2e30781bec
helloooooo <yomicerisier@gmail.com>
2025-06-25 06:57:50 +0900

    暗記パターン

--- neetcode/arai60/number_of_islands.py
+++ neetcode/arai60/number_of_islands.py
@@ -3,7 +3,7 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
-            return 0
+            return False
         rows, cols = len(grid), len(grid[0])
         visited = [[False] * cols for _ in range(rows)]
         count = 0
@@ -14,13 +14,13 @@ class Solution:
                     self.bfs(grid, visited, i, j)
                     count += 1
         return count
-    
-    def bfs(self, grid: List[List[str]], visited: List[List[str]], i: int, j: int):
+
+    def bfs(self, grid: List[List[str]], visited: List[List[bool]], i: int, j: int):
         rows, cols = len(grid), len(grid[0])
-        queue = deque([(i,j)])
+        queue = deque([(i, j)])
         while queue:
             x, y = queue.popleft()
             if 0 <= x < rows and 0 <= y < cols and grid[x][y] == '1' and not visited[x][y]:
                 visited[x][y] = True
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                for dx, dy in [(-1, 0), (1 , 0), (0, -1), (0, 1)]:
                     queue.append((x + dx, y + dy))
