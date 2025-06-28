ba1759697482230a26bc9779951c66c677db6c3b
helloooooo <yomicerisier@gmail.com>
2025-06-29 08:30:50 +0900

    scratch

--- neetcode/arai60/word_ladder.py
+++ neetcode/arai60/word_ladder.py
@@ -5,9 +5,7 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-
         word_set = set(wordList)
-
         queue = deque([(beginWord, 1)])
 
         visited = set([beginWord])
@@ -17,16 +15,15 @@ class Solution:
 
             if current_word == endWord:
                 return steps
-
+            
             for i in range(len(current_word)):
                 for c in 'abcdefghijklmnopqrstuvwxyz':
                     if c == current_word[i]:
                         continue
-
+                    
                     new_word = current_word[:i] + c + current_word[i+1:]
 
                     if new_word in word_set and new_word not in visited:
                         visited.add(new_word)
-                        queue.append((new_word, steps + 1))
+                        queue.append([new_word, steps + 1])
         return 0
-
